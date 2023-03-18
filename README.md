5e4d7abf91bb7b80349f492ba73d998d59b76ed4
Abdul Jaweed <jdgaming7320@gmail.com>
2023-03-18 18:06:21 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Regex-Clone
+# Regex Clone
+
+ This Flask app clones the core functionality of regex101.com. It matches a test string with a regex and displays all the matches. Built with Flask and Python's regular expression module, it provides a simple and efficient way to test regular expressions.
+
+### Tech Used
+
+- Python
+- Flask
---
+++ app.py
@@ -0,0 +1,22 @@
+import re
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index_func():
+    return render_template("index.html")
+
+@app.route('/',methods=['GET','POST'])
+def output_func():
+    if request.method == 'POST':
+        regex=request.form['in_1']
+        string=request.form['in_2']
+        match=[(ele.start(), ele.end()) for ele in re.finditer(regex,string,flags=re.IGNORECASE)]
+        count=len(match)
+
+    return render_template('output.html',r=regex,s=string,match=match,count=count)
+
+if __name__ == '__main__':
+    app.run(debug=True)
